fa7a9dcde20fd826a61e4136d50b3bea03616ad9
jezzamondev <jezzamondev@gmail.com>
2019-08-21 16:04:29 -0400

    complete component set up tests

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,16 @@
-import { configure, setAddon, addDecorator } from '@storybook/react';
+import { configure, setAddon, addDecorator, addParameters } from '@storybook/react';
 import JSXAddon from 'storybook-addon-jsx';
 import { withInfo } from '@storybook/addon-info';
 import { withKnobs } from '@storybook/addon-knobs/react';
 import { muiTheme } from 'storybook-addon-material-ui';
 import darkTheme from '../src/darkTheme';
+import { themes } from '@storybook/theming';
+
+addParameters({
+  options: {
+    theme: themes.dark,
+  },
+});
 
 addDecorator(withInfo);
 addDecorator(withKnobs);
